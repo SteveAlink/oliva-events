4107bfcdbf1ce08f74e39e7ef68b0d2bb30f9a6e
SteveAlink <50744139+SteveAlink@users.noreply.github.com>
2026-05-01 19:04:09 +0200

    Add oliva-events plugin for WonderCMS

---
+++ oliva-events.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * oliva-events - Events plugin for WonderCMS.
+ * Prepared by Steve Alink for Oliva Solutions
+ *
+ * Shows one or more calendars with events or availability
+ */
+
+if (!defined('VERSION')) {
+    die('Direct access denied');
+}
+
+require_once __DIR__ . '/class.oliva-events.php';
+
+$olivaEvents = new OlivaEvents($Wcms);
+
+if (isset($Wcms)) {
+    $Wcms->addListener('css', 'olivaEventsCss');
+    $Wcms->addListener('js', 'olivaEventsJs');
+    $Wcms->addListener('settings', [$olivaEvents, 'handleSettings']);
+    $Wcms->addListener('footer', [$olivaEvents, 'renderCalendar']);
+} elseif (class_exists('wCMS')) {
+    wCMS::addListener('css', 'olivaEventsCss');
+    wCMS::addListener('js', 'olivaEventsJs');
+    wCMS::addListener('settings', [$olivaEvents, 'handleSettings']);
+    wCMS::addListener('footer', [$olivaEvents, 'renderCalendar']);
+}
+
+function olivaEventsPluginBasePath()
+{
+    return 'plugins/oliva-events/';
+}
+
+function olivaEventsCss($args)
+{
+    $css = '<link rel="stylesheet" href="' . olivaEventsPluginBasePath() . 'css/style.css">' . PHP_EOL;
+
+    if (isset($args[0]) && is_array($args[0])) {
+        $args[0][] = $css;
+    } else {
+        $args[0] = ($args[0] ?? '') . $css;
+    }
+
+    return $args;
+}
+
+function olivaEventsJs($args)
+{
+    $js = '<script src="' . olivaEventsPluginBasePath() . 'js/oliva-events.js"></script>' . PHP_EOL;
+
+    if (isset($args[0]) && is_array($args[0])) {
+        $args[0][] = $js;
+    } else {
+        $args[0] = ($args[0] ?? '') . $js;
+    }
+
+    return $args;
+}
